94e170fec671646e3a331ef04763b65857c2a5cd
sulton0011 <azizxonkom@gmail.com>
2021-12-22 21:47:11 +0500

    Total amount of points 0.3

--- Total_amount_of_points.go
+++ Total_amount_of_points.go
@@ -1,12 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
-func main () {
-	fmt.Println(Points([]string{"1:1","2:2","3:3","4:4","2:2","3:3","4:4","3:3","4:4","4:4"}))
-}
 func Points(games []string) (sum int) {
 	for _, i := range games {
 		if i[0] > i[2]{
