18ff077ff88ad1c0d989bd74d58be2ed3d2994e1
sulton0011 <azizxonkom@gmail.com>
2021-12-22 14:47:46 +0500

    String repeat 0.2

--- String_repeat.go
+++ String_repeat.go
@@ -1,11 +1,4 @@
 package main
-import (
-	"fmt"
-)
-
-func main () {
-	fmt.Println(RepeatStr(4, "Salom"))
-}
 
 func RepeatStr(repetitions int, value string)(s string) {
 	for i := 0; i < repetitions; i ++ {
