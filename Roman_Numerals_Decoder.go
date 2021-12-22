660d9816980afec165e537cb573edbf52a06cebe
sulton0011 <azizxonkom@gmail.com>
2021-12-22 22:10:30 +0500

    Roman Numerals Decoder 0.1

--- Roman_Numerals_Decoder.go
+++ Roman_Numerals_Decoder.go
@@ -5,6 +5,19 @@ import (
 func main () {
 	fmt.Println(Decode("XXI"))
 }
-func Decode(roman string) int {
-	return 0
+func Decode(roman string) (sum int) {
+	RimSon := map[string]int{
+		"I" : 1,
+		"V" : 5,
+		"X" : 10,
+		"L" : 50,
+		"C" : 100,
+		"D" : 500,
+		"M" : 1000,
+	}
+	for i := len(roman) - 1; i >= 0; i -- {
+		sum += RimSon[string(roman[i])]
+	}
+
+	return 
 }
