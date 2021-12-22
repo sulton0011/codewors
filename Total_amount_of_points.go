0a402b925b7602fa566376612aaff398de3f31b0
sulton0011 <azizxonkom@gmail.com>
2021-12-22 21:46:59 +0500

    Total amount of points 0.2

--- Total_amount_of_points.go
+++ Total_amount_of_points.go
@@ -2,22 +2,16 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-
 )
 
 func main () {
 	fmt.Println(Points([]string{"1:1","2:2","3:3","4:4","2:2","3:3","4:4","3:3","4:4","4:4"}))
 }
 func Points(games []string) (sum int) {
-	for i := range games {
-		s, _ := strconv.Atoi(string(games[i][0]))
-		a, _ := strconv.Atoi(string(games[i][2]))
-		if s > a {
+	for _, i := range games {
+		if i[0] > i[2]{
 			sum += 3
-		}else if s < a {
-			sum += 0
-		}else {
+		}else if i[0] == i[2] {
 			sum += 1
 		}
 	}
