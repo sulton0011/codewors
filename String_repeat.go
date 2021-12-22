1a228fcaddc992577eb9114cca54c0e740adf0ca
sulton0011 <azizxonkom@gmail.com>
2021-12-22 14:47:08 +0500

    String repeat 0.1

--- String_repeat.go
+++ String_repeat.go
@@ -4,9 +4,12 @@ import (
 )
 
 func main () {
-	fmt.Println(RepeatStr(4, "a"))
+	fmt.Println(RepeatStr(4, "Salom"))
 }
 
-func RepeatStr(repetitions int, value string) string {
-	return ""
+func RepeatStr(repetitions int, value string)(s string) {
+	for i := 0; i < repetitions; i ++ {
+		s += value
+	}
+	return s
 }
