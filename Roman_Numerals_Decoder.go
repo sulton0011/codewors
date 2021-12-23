fdd8d1426af41be48e8253528fdff7aec9ef7de4
sulton0011 <azizxonkom@gmail.com>
2021-12-23 12:05:20 +0500

    Roman Numerals Decoder 0.3

--- Roman_Numerals_Decoder.go
+++ Roman_Numerals_Decoder.go
@@ -7,30 +7,21 @@ import (
 func main () {
 	fmt.Println(Decode("MMVIII"))
 }
-func Decode(roman string) (sum int) {
-	RimSon := map[string]int{
-		"I" : 1,
-		"V" : 5,
-		"X" : 10,
-		"L" : 50,
-		"C" : 100,
-		"D" : 500,
-		"M" : 1000,
+func Decode(roman string) int {
+	numerals := map[rune]int{
+	  'I': 1,
+	  'V': 5,
+	  'X': 10,
+	  'L': 50,
+	  'C': 100,
+	  'D': 500,
+	  'M': 1000,
 	}
-	for i := 0; i < len(roman); i ++ {
-		var a bool
-		if i < len(roman) - 2{
-			a = (RimSon[string(roman[i + 1])] > RimSon[string(roman[i])] || RimSon[string(roman[i + 2])] > RimSon[string(roman[i])])
-			}else if i < len(roman) - 1{
-			a = (RimSon[string(roman[i + 1])] > RimSon[string(roman[i])])
-		}
-		
-		if a {
-			sum -= RimSon[string(roman[i])]
-		}else {
-			sum += RimSon[string(roman[i])]
-		}
+	
+	var result int
+	for _, v := range roman {
+	  result += numerals[v] - (result % numerals[v])*2
 	}
-
-	return sum
-}
+	
+	return result
+  }
