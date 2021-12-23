58e80e6169078b9a81db42737fd4f8e7e8e8e045
sulton0011 <azizxonkom@gmail.com>
2021-12-23 12:55:43 +0500

    Decode the Morse code 0.2

--- Decode_the_Morse_code.go
+++ Decode_the_Morse_code.go
@@ -1,6 +1,8 @@
 package main
+
 import (
 	"fmt"
+	"strings"
 )
 func main () {
 	fmt.Println(DecodeMorse(".--"))
@@ -17,9 +19,12 @@ func DecodeMorse(morseCode string) string {
 		"..-" : "U", "...-" : "V", ".--" : "W", "-..-" : "X",
 		"-.--" : "Y", "--.." : "Z", "-----" : "0", ".----" : "1",
 		"..---" : "2", "...--" : "3", "....-" : "4", "....." : "5",
-		"-...." : "6", "--..." : "7", "---.." : "8", "----." : "9"
+		"-...." : "6", "--..." : "7", "---.." : "8", "----." : "9",
 	}
-	return ""
+	for i, j := range Morse {
+		morseCode = strings.ReplaceAll(morseCode, i, j)
+	}
+	return morseCode
   }
   
   
