91b7ada8a68ca835d73a62a74d81f6397001870b
sulton0011 <azizxonkom@gmail.com>
2021-12-23 14:33:37 +0500

    Decode the Morse code 0.6

--- Decode_the_Morse_code.go
+++ Decode_the_Morse_code.go
@@ -1,24 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"strings"
 )
-func main () {
-	fmt.Println(DecodeMorse(".... . -.--   .--- ..- -.. ."))
-}
 
 func DecodeMorse(morseCode string) (decoded string) {
-	fmt.Println(strings.Split(strings.TrimSpace(morseCode), "   "))
 	for _, word := range strings.Split(strings.TrimSpace(morseCode), "   ") {
 		for _, char := range strings.Split(word, " "){
-			fmt.Println(char)
-		  decoded += char
+		  decoded += MORSE_CODE[char]
 		}
 		decoded += " "
 	  }
-	  return strings.TrimSpace(decoded)
-	
+	  return strings.TrimSpace(decoded)	
 }
   
   
